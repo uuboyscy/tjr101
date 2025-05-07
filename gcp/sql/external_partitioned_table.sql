e86d6e1b5fbc7d46119ef151f6c66ca19fced1db
uuboyscy <aegis12321@gmail.com>
2025-05-07 19:07:14 +0800

    Add SQL for creating external table from Google Sheets

--- gcp/sql/external_partitioned_table.sql
+++ gcp/sql/external_partitioned_table.sql
@@ -1,3 +1,4 @@
+-- Create partitioned external table from files in GCS
 CREATE OR REPLACE EXTERNAL TABLE `<your_dataset>.SalePartitionExternal`
 (
   TransactionID STRING,
@@ -12,4 +13,13 @@ OPTIONS (
   hive_partition_uri_prefix = 'gs://<your_bucket>/<external_partitioned_folder>',
   skip_leading_rows = 1,
   max_bad_records = 1
+);
+
+-- Create external table from Google Sheets
+CREATE OR REPLACE EXTERNAL TABLE `<your_dataset>.<your_table_name>`
+OPTIONS (
+  format = 'GOOGLE_SHEETS',
+  skip_leading_rows = 1,
+  uris = ['<your_googlesheets_url>'],
+  sheet_range = '<sheet_name>!A:D'
 );
